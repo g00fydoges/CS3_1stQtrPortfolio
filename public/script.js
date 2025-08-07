8bf0a128969188eaa1dee18d1ac84c4496070307
g00fydoges <b2029rgregala@pshs.edu,ph>
2025-08-07 10:33:34 +0800

    nongraded

---
+++ public/script.js
@@ -0,0 +1,6 @@
+let age = 6;
+let maxAge = 67;
+let numPerDay = 7;
+let totalRequired = (numPerDay * 365) * (maxAge - age);
+
+console.log(" You will need " + totalRequired + " to last you until the ripe old age of " + maxAge + ".");
